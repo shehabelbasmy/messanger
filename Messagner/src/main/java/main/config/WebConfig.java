eaf627b1d69b45192745204a79cbde4cfa42181b
Shehab <Shehab@192.168.1.24>
2022-09-10 22:28:30 +0200

    add webconfig for configure some viewcontroller

---
+++ Messagner/src/main/java/main/config/WebConfig.java
@@ -0,0 +1,19 @@
+package main.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/signin").setViewName("signin");
+		registry.addViewController("/signup").setViewName("signup");
+		registry.addViewController("/").setViewName("../index");
+	}
+	
+}
